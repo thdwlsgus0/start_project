a974cf46759d477246adc4813fb8009b6e390714
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-11-06 15:45:06 +0900

    feat: 로고 추가(#12)

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -19,14 +19,38 @@ const StyledMenuLink = styled.a`
 	}
 `;
 
+const StyledMenuNav = styled.div`
+	display: flex;
+	padding: 0 20px;
+`;
+
+const StyledMenuHeading = styled.h1`
+	padding: 28px 0; 
+`;
+
+const StyledHeadingLink = styled.a`
+	display: block;
+	position: relative;
+	padding: 0 10px;
+	font-size: 16px;
+	line-height: 19px;
+	letter-spacing: -1px;
+	color: #dce384;
+`;
+
 const Header = () => {
 	return (
-		<StyledContainer>
-			<StyledMenuLink href="#">기관안내</StyledMenuLink>
-			<StyledMenuLink href="#">오시는길</StyledMenuLink>
-			<StyledMenuLink href="#">게시판</StyledMenuLink>
-			<StyledMenuLink href="#">활동안내</StyledMenuLink>
-		</StyledContainer>
+		<StyledMenuNav>
+			<StyledMenuHeading>
+				<StyledHeadingLink href="#">아산방문요양 기관</StyledHeadingLink>
+			</StyledMenuHeading>
+			<StyledContainer>
+				<StyledMenuLink href="#">기관안내</StyledMenuLink>
+				<StyledMenuLink href="#">오시는길</StyledMenuLink>
+				<StyledMenuLink href="#">게시판</StyledMenuLink>
+				<StyledMenuLink href="#">활동안내</StyledMenuLink>
+			</StyledContainer>
+		</StyledMenuNav>
 	)
 };
 
