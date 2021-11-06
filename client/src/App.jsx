aaff5686c70545c25874d2552c417ef8efe578f6
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-11-06 15:44:44 +0900

    feat: global style 설정(#12)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+	  padding: 0;
+	  margin: 0;
+  }
+
+  a {
+	  text-decoration: none;
+  }
+
+  h1 {
+	  margin: 0;
+	  padding: 0;
+  }
+`;
 
 const App = () => {
 	return (
-		<Header></Header>
+		<>
+			<GlobalStyle/>
+			<Header></Header>
+		</>
 	)
 }
 
