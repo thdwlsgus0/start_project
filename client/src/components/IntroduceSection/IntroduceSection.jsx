d4b2b0490cfdedbe5d4cb5490ebbe8c9f66a5785
thdwlsgus0 <thdwlsgus0@naver.com>
2021-12-25 17:51:07 +0900

    style: 소개 컴포넌트 더 추가(#16)

--- client/src/components/IntroduceSection/IntroduceSection.jsx
+++ client/src/components/IntroduceSection/IntroduceSection.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import KeyboardArrowRight from '@images/keyboard_arrow_right.svg';
 
 const IntroduceSectionContainer = styled.div`
 	text-align: center;
-
-	&:not(:first-child) {
-		margin-top: 80px;
-	}
 `;
 
 const IntroduceSectionTitle = styled.strong`
@@ -29,12 +26,95 @@ const IntroduceSectionSubTitle = styled.span`
 	}
 `;
 
+const AnnounceSection = styled.div`
+	&:not(:first-child) {
+		margin-top: 80px;
+	}
+`;
+
+const AnnounceSectionContainer = styled.div`
+	display: flex;
+	padding-bottom: 10px;
+`;
+
+const AnnounceSectionTitle = styled.strong`
+	font-size: 20px;
+	letter-spacing: -0.8px;
+	color: #dce384;
+	display: block;
+	flex: 1 1 auto;
+
+	&:not(:last-child) {
+		padding-top: 10px;
+	}
+`;
+
+const AnnounceSectionLink = styled.a`
+	font-size: 15px;
+	color: #666;
+	cursor: pointer;
+
+	&:after {
+		position: relative;
+		top: 6px;
+		content: '';
+		display: inline-block;
+		width: 24px;
+		height: 24px;
+		background: url(${(props) => props.image}) no-repeat;
+	}
+`;
+
+const IntroContainer = styled.div`
+	padding: 0 90px;
+
+	&:not(:first-child) {
+		margin-top: 80px;
+	}
+`;
+
+const AnnounceItem = styled.div`
+	display: flex;
+	padding: 20px 0;
+
+	&:not(:first-child) {
+		border-top: 1px solid #A4A4A4;
+	}
+`;
+
+const AnnounceItemData = styled.span`
+	display: block;
+	font-size: 15px;
+	line-height: 22px;
+	color: #999;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+`;
+
 const IntroduceSection = () => {
 	return (
-		<IntroduceSectionContainer>
-			<IntroduceSectionTitle>아산대표 방문요양 • 노인성질환전문케어</IntroduceSectionTitle>
-			<IntroduceSectionSubTitle> 방문요양 제공 / 국가지원 안내 / 코로나 검사시행 및 상시관리 기관</IntroduceSectionSubTitle>
-		</IntroduceSectionContainer>
+		<IntroContainer>
+			<IntroduceSectionContainer>
+				<IntroduceSectionTitle>아산대표 방문요양 • 노인성질환전문케어</IntroduceSectionTitle>
+				<IntroduceSectionSubTitle> 방문요양 제공 / 국가지원 안내 / 코로나 검사시행 및 상시관리 기관</IntroduceSectionSubTitle>
+			</IntroduceSectionContainer>
+			<AnnounceSection>
+				<AnnounceSectionContainer>
+					<AnnounceSectionTitle>기관안내</AnnounceSectionTitle>
+					<AnnounceSectionLink image={KeyboardArrowRight}>더보기</AnnounceSectionLink>
+				</AnnounceSectionContainer>
+				<AnnounceItem>
+					<AnnounceItemData>아산재가복지센터 041-531-7877</AnnounceItemData>
+				</AnnounceItem>
+				<AnnounceItem>
+					<AnnounceItemData>• 아산대표 방문요양 노인복지기관/ 대표: 김정남</AnnounceItemData>
+				</AnnounceItem>
+				<AnnounceItem>
+					<AnnounceItemData>• 어르신댁에 요양보호사님을 보내드립니다.</AnnounceItemData>
+				</AnnounceItem>
+			</AnnounceSection>
+		</IntroContainer>
 	);
 }
 
